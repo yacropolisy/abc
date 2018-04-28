e1cc4b9336bb88813b2cdae2a8d0b26b1f3a9061
Yoshinobu Yamada <cancer_acropolis@yahoo.co.jp>
2018-04-28 21:50:11 +0900

    48 AC trying easy case is important!

---
+++ 48d.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <cmath>
+#include <iomanip>
+#include <map>
+#include <deque>
+using namespace std;
+
+#define REP(i,n) for(int i = 0; i < n; i++)
+#define ALL(v) v.begin(),v.end()
+typedef long long ll;
+typedef pair<int,int> pi;
+typedef pair<ll,ll> pll;
+typedef pair<string,string> pss;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<pi> vpi;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+
+double EPS = 1e-9;
+int INFi = 1000000005;
+long long INFll = 1000000000000000005ll;
+double PI = acos(-1);
+int dirx[8] = {-1,0,0,1,-1,-1,1,1};
+int diry[8] = {0,1,-1,0,-1,1,-1,1};
+ll MOD = 1000000007;
+
+int main(){
+  string s;
+  cin >> s;
+  bool flag = true;
+  if(s[0]==s[s.size()-1])flag = !flag;
+  if(s.size() %2 == 0)flag = !flag;
+  if(flag) cout << "First" << endl;
+  else cout << "Second" << endl;
+
+  return 0;
+}
