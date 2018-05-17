7b0b2f18caf5d3842117ea13fcc4387a323c8b5b
Yoshinobu Yamada <cancer_acropolis@yahoo.co.jp>
2018-05-17 18:50:10 +0900

    29 AC

---
+++ 29d.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <cmath>
+#include <iomanip>
+#include <map>
+#include <deque>
+using namespace std;
+
+#define REP(i,n) for(int i = 0; i < n; i++)
+#define ALL(v) v.begin(),v.end()
+typedef long long ll;
+typedef pair<int,int> pi;
+typedef pair<ll,ll> pll;
+typedef pair<string,string> pss;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<pi> vpi;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+
+double EPS = 1e-9;
+int INFi = 1000000005;
+long long INFll = 1000000000000000005ll;
+double PI = acos(-1);
+int dirx[8] = {-1,0,0,1,-1,-1,1,1};
+int diry[8] = {0,1,-1,0,-1,1,-1,1};
+ll MOD = 1000000007;
+
+int main(){
+  ll n;
+  cin >> n;
+
+  ll ans = 0;
+  ll ct = 1;
+  while(n / ct > 0){
+    ct *= 10;
+    ans += n / ct * (ct / 10);
+    ans += max(0, int(min(n % ct+1, 2 * ct/10) - ct/10));
+  }
+
+  cout << ans << endl;
+
+  return 0;
+}
