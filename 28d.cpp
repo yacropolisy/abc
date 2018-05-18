d54b7749033d3cdcad1251a571031e2b46f6eb1b
Yoshinobu Yamada <cancer_acropolis@yahoo.co.jp>
2018-05-18 12:47:01 +0900

    28 AC easy

---
+++ 28d.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <cmath>
+#include <iomanip>
+#include <map>
+#include <deque>
+using namespace std;
+
+#define REP(i,n) for(int i = 0; i < n; i++)
+#define ALL(v) v.begin(),v.end()
+typedef long long ll;
+typedef pair<int,int> pi;
+typedef pair<ll,ll> pll;
+typedef pair<string,string> pss;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<pi> vpi;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+
+double EPS = 1e-9;
+int INFi = 1000000005;
+long long INFll = 1000000000000000005ll;
+double PI = acos(-1);
+int dirx[8] = {-1,0,0,1,-1,-1,1,1};
+int diry[8] = {0,1,-1,0,-1,1,-1,1};
+ll MOD = 1000000007;
+
+int main(){
+  ll n, k;
+  cin >> n >> k;
+
+  cout.precision(20);
+  cout << double(6 * (k-1) * (n-k) + 3 * (n-k) + 3 * (k-1) + 1) / (n * n * n)  << endl;
+
+  return 0;
+}
